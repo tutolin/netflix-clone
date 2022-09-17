7953feb64ea7d46cc10634ff936a7984211dc76a
Tolulope Aboyeji <aboyejitolu@gmail.com>
2022-09-17 19:00:21 +0100

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Tolulope Aboyeji on 17/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
