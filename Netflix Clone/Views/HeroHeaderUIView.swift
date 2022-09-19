10de41e0c3f3a44c292f2838954fb6ab474c00e4
Tolulope Aboyeji <aboyejitolu@gmail.com>
2022-09-19 23:33:44 +0100

    remove init frame

--- Netflix Clone/Views/HeroHeaderUIView.swift
+++ Netflix Clone/Views/HeroHeaderUIView.swift
@@ -50,9 +50,7 @@ class HeroHeaderUIView: UIView {
         layer.addSublayer(gradientLayer)
     }
 
-    override init(frame: CGRect) {
-        <#code#>
-    }
+   
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -60,6 +58,6 @@ class HeroHeaderUIView: UIView {
     
     private func applyConstraints() {
         
-    }
+     }
     
 }
