7f00510ff80204961586d42967234524e4a047a0
Tolulope Aboyeji <aboyejitolu@gmail.com>
2022-09-20 23:24:44 +0100

    update

--- Netflix Clone/Views/HeroHeaderUIView.swift
+++ Netflix Clone/Views/HeroHeaderUIView.swift
@@ -50,7 +50,8 @@ class HeroHeaderUIView: UIView {
         layer.addSublayer(gradientLayer)
     }
 
-   
+    override init(frame: CGRect) {
+    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
