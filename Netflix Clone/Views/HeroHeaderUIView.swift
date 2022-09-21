cb79053fb79fbafd3a5a1c542894a287b3840838
Tolulope Aboyeji <aboyejitolu@gmail.com>
2022-09-21 23:45:38 +0100

    update

--- Netflix Clone/Views/HeroHeaderUIView.swift
+++ Netflix Clone/Views/HeroHeaderUIView.swift
@@ -14,7 +14,7 @@ class HeroHeaderUIView: UIView {
         let button = UIButton()
         button.setTitle("Download", for: .normal)
         button.layer.borderColor = UIColor.white.cgColor
-        button.layer.borderWidth = 1
+        button.layer.borderWidth = 2
         button.translatesAutoresizingMaskIntoConstraints = false
         button.layer.cornerRadius = 5
         return button
